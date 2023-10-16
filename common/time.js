6e01cd1b347bed4f64699dd81b364559ad79ce20
k-im-minsu <upwn1655@naver.com>
2023-10-17 01:33:03 +0900

    Refactor: time script add, function that return increased or decreased time refactor, time_tick function change algorithm

--- common/time.js
+++ common/time.js
@@ -59,37 +59,40 @@ exports.time_view=(format='YYYY/MM/DD HH:mm:ss',tick=undefined,add_type='',add_n
 }
 /**
  * get present time tick of total millisecond
- * @param {string} add_type type of time to be added [y, M, w, d, h, m ,s, ms]
+ * @param {string|null} add_type type of time to be added [y, M, w, d, h, m ,s, ms]
  * @param {number} add_num number of time to be added
- * @param {boolean} is_utc is true if $tick is time of utc
  * @returns {number} total milliseconds
  */
-exports.time_tick=(add_type='',add_num=0,is_utc=true) =>{  
-    switch(add_type){
-    case 'y':
-    case 'M':
-    case 'w':
-    case 'd':   
-    case 'h':
-    case 'm':
-    case 's':
-    case 'ms':
-        if(add_num==0){
-            add_type='';
+exports.time_tick=(add_type=null,add_num=0) =>{  
+    if(!add_type){
+        return moment().valueOf();
+    }else{
+        switch(add_type){
+        case 'y':
+        case 'M':
+        case 'w':
+        case 'd':   
+        case 'h':
+        case 'm':
+        case 's':
+        case 'ms':
+            if(!add_num){
+                return moment().valueOf();
+            }else{
+                return moment().add(add_num,add_type).valueOf();
+            }
+        default :
+        return moment().valueOf();
         }
-        break;
-    default :
-        break;
-}
-if(is_utc){
-    if(add_type.length==0)
-    return moment.utc().valueOf();
-    else
-    return moment.utc().add(add_num,add_type).valueOf();
-}else{
-    if(add_type.length==0)
-    return moment().valueOf();
-    else
-    return moment().add(add_num,add_type).valueOf();
-}
+    }
 };
+/**
+ * get increase or decrease time tick of total millisecond
+ * @param {number} old_tick base time tick(ms)
+ * @param {string} add_type type of time to be added [y, M, w, d, h, m ,s, ms]
+ * @param {number} add_num number of time to be added
+ * @returns {number} new total milliseconds
+ */
+exports.time_tick_add=(old_tick,add_type,add_num)=>{
+    return moment(old_tick).add(add_num,add_type).valueOf();
+}
